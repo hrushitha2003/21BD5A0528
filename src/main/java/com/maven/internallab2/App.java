57730ed8796840138c366c5f30410851fa2094c2
hrushitha <hrushithachinthakayalac@gmail.com>
2022-12-15 15:34:44 +0530

    labinternal

---
+++ src/main/java/com/maven/internallab2/App.java
@@ -0,0 +1,13 @@
+package com.maven.internallab2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
